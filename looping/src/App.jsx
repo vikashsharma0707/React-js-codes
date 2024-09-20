551229ef4d0e813e9bef7e530adf9077c5050dc5
vikash sharma <vikashtiwari9893@gmail.com>
2024-09-21 01:47:09 +0530

    Looping in react js

---
+++ looping/src/App.jsx
@@ -0,0 +1,39 @@
+import data from "../db/db.json"
+
+
+const App=()=>{
+
+ let ans= data.map((key)=>{
+    return(
+        <>
+        <h1>{key.id}</h1>
+        <h1>{key.name}</h1>
+        <img src={key.image}  style={{width:"100px",height:"100px"}}/>
+        <h4>{key.rating}</h4>
+        <p>{key.discription}</p>
+        <a href={key.watch} ><button>Watch now</button></a>
+        
+        </>
+
+    )
+
+ })
+
+    return(
+        <>
+
+        <h1 style={{alignItems:"center",justifyContent:"center",marginLeft:"40%"}}>This is Looping in React JSX</h1>
+        
+        <div style={{display:"flex",justifyContent:"space-evenly"}}>
+            {ans}
+        </div>
+
+
+        
+        
+        </>
+    )
+}
+
+
+export default App;
